258063584762b7d4d813f04687b4a179bad4bac4
David Dvořák <dvorakdavid7@gmail.com>
2022-11-25 16:17:35 +0100

    init

---
+++ src/main/java/cz/csob/busmon/server/ServerApplication.java
@@ -0,0 +1,11 @@
+package cz.csob.busmon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
